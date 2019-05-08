53b693bb7e8129ab28c7b58df2156ed11ebcbed0
amanoooo <amanoooooooooooooooo@gmail.com>
2019-05-08 15:46:07 +0800

    add webpcak-demo

---
+++ webpack-demo/src/index.js
@@ -0,0 +1,33 @@
+// import _ from 'lodash'
+
+// function component () {
+//   var element = document.createElement('div')
+
+//   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+//   return element
+// }
+
+// document.body.appendChild(component())
+
+import { cube } from './math.js'
+import net from 'net'
+import fs from 'fs'
+import Buffer from 'Buffer'
+
+console.log('net is ', net)
+console.log('fs is ', fs)
+console.log('Buffer is ', Buffer)
+
+function component () {
+  var element = document.createElement('pre')
+
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n')
+
+  return element
+}
+document.body.appendChild(component())
---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  node: {
+    fs: 'empty',
+    net: 'mock',
+    Buffer: 'mock'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: false, // Note `mangle.properties` is `false` by default.
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_fnames: true
+        }
+      })
+    ]
+  }
+}
